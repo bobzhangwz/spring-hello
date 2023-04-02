2f1990b1ac220165b1de932601f7d8661578c2df
BO ZHANG <bozhang@thoughtworks.com>
2023-04-02 21:38:30 +0800

    init project

---
+++ app/src/main/java/com/zhpooer/demo/app/App.java
@@ -0,0 +1,11 @@
+package com.zhpooer.demo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
