514e82d9380fe4bcd96635acdd5a886cf720f781
BO ZHANG <bozhang@thoughtworks.com>
2023-04-03 08:38:54 +0800

    test

--- app/src/main/java/com/zhpooer/demo/app/HelloWorld.java
+++ app/src/main/java/com/zhpooer/demo/app/HelloWorld.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorld {
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+        return String.format("Hello %s!!!", name);
     }
 }
